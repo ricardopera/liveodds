8794d210cd6fee99757c4e041daea074bc0d5a51
4a47 <noplundy@gmail.com>
2019-05-20 21:41:49 +0100

    update setup version

---
+++ liveodds/__init__.py
@@ -0,0 +1,1 @@
+from liveodds import *
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-import setuptools
+from distutils.core import setup
 
-
-setuptools.setup(
-    name='liveodds',  
-    version='0.1',
+setup(
+    name = 'liveodds',
+    packages = ['liveodds'],
+    version='0.0.1',
     author="JG",
     description="A library for live horse racing odds in UK and Ireland",
     author_email="noplundy@gmail.com",
     url="https://github.com/4A47/liveodds",
-    py_modules=['liveodds']
- )
+    download_url = 'https://github.com/4A47/liveodds/archive/v0.0.1.tar.gz',
+    keywords = ['Horse Racing', 'Live Odds', 'Betting'],
+    install_requires=[
+          'lxml',
+          'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    )
