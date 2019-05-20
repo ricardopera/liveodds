a35a266030d7382219d23087b47f1b5e87a09423
4a47 <noplundy@gmail.com>
2019-05-20 21:05:59 +0100

    update structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name='liveodds',  
+    version='0.1',
+    author="JG",
+    description="A library for live horse racing odds in UK and Ireland",
+    author_email="noplundy@gmail.com",
+    url="https://github.com/4A47/liveodds",
+    py_modules=['liveodds']
+ )
